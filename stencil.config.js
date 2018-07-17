c11cc3cb20ab6868c923c2cc4689ad111604f5a7
Nicolás Cardelino <ncardeli@gmail.com>
2018-07-17 14:31:29 -0300

    Include Bootstrap library CSS in controls
    Now every Bootstrap based control includes the Bootstrap CSS code.
    This allows each control to work without depending on the developer to explicitly include Bootstrap.

--- stencil.config.js
+++ stencil.config.js
@@ -1,6 +1,12 @@
 const sass = require("@stencil/sass");
 
 exports.config = {
+  copy: [
+    {
+      src: "../node_modules/font-awesome/fonts",
+      dest: "bootstrap/font-awesome/fonts"
+    }
+  ],
   namespace: "gx-web-controls",
   outputTargets: [
     {
