cae031454a7d2db19da95ef180afcd2f59e14f9d
Nicolás Cardelino <ncardeli@gmail.com>
2018-08-17 19:16:20 -0300

    Code formatting

--- src/components/canvas-cell/canvas-cell.tsx
+++ src/components/canvas-cell/canvas-cell.tsx
@@ -16,12 +16,12 @@ export class CanvasCell extends BaseComponent {
 
   /**
    * This attribute defines how the control behaves when the content overflows.
-   * 
+   *
    * | Value    | Details                                                     |
    * | -------- | ----------------------------------------------------------- |
    * | `scroll` | The overflowin content is hidden, but scrollbars are shown  |
    * | `clip`   | The overflowing content is hidden, without scrollbars       |
-   * 
+   *
    */
   @Prop() overflowMode: "scroll" | "clip";
 
