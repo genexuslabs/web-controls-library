d5dc274513280596f8f781de81f00908d2be0bc1
Nicolás Cardelino <ncardeli@gmail.com>
2018-11-21 16:37:28 -0300

    Remove gx-layout-editor component
    gx-layout-editor has its own package now: [@genexus/layout-editor-component](https://www.npmjs.com/package/@genexus/layout-editor-component)

--- src/components.d.ts
+++ src/components.d.ts
@@ -454,46 +454,6 @@ export namespace Components {
     width?: string;
   }
 
-  interface GxLayoutEditorPlaceholder {}
-  interface GxLayoutEditorPlaceholderAttributes extends StencilHTMLAttributes {}
-
-  interface GxLayoutEditor {
-    /**
-     * The abstract form model object
-     */
-    model: any;
-    /**
-     * Array with the identifiers of the selected controls. If empty the whole layout-editor is marked as selected.
-     */
-    selectedControls: string[];
-  }
-  interface GxLayoutEditorAttributes extends StencilHTMLAttributes {
-    /**
-     * The abstract form model object
-     */
-    model?: any;
-    /**
-     * Fired when a control (that wasn't already inside the layout editor) has been dropped on a valid drop target (for example, a control from a toolbox or an object from the knowledge base navigator)  ##### Dragging a control  If a control is being dragged, the dataTransfer property of the event must have the following format:  `"GX_DASHBOARD_ADDELEMENT,[GeneXus type of control]"`  where:  * `GX_DASHBOARD_ADDELEMENT` is the type of action * `[GeneXus type of control]` is the type of control that's been added. This value can have any value and will be passed as part of the information sent as part of the event.  ##### Dragging a KB object  If a KB object is being dragged, the dataTransfer property of the event must contain the name of the KB object.  ##### Dropped control information  An object containing information of the add operation is sent in the `detail` property of the event object.  If a KB object was dropped, the following properties are set:  | Property          | Details                                                                                                                                     | | ----------------- | ------------------------------------------------------------------------------------------------------------------------------------------- | | `kbObjectName`    | Name of the GeneXus object                                                                                                               |  If control was dropped, the following properties are set.  | Property          | Details                                                                                                                                     | | ----------------- | ------------------------------------------------------------------------------------------------------------------------------------------- | | `elementType`     | The type of the control that's been added and was received as the `[GeneXus type of control]` in the dataTransfer of the drop operation     |  Depending on where the control was dropped, additional information will be provided and different properties will be set. There are four possible cases:  1. Dropped on an empty container or in the last row of a container 2. Dropped on a new row of a non empty container 3. Dropped on an existing empty cell 4. Dropped on an existing row   ###### 1. Dropped on an empty container or on a new row that will be the last row of a container  | Property          | Details                                                                                                                                     | | ----------------- | ------------------------------------------------------------------------------------------------------------------------------------------- | | `containerId`     | Identifier of the container where the control was dropped                                                                                   |  ###### 2. Dropped on a new row of a non empty container  | Property          | Details                                                                                                                                     | | ----------------- | ------------------------------------------------------------------------------------------------------------------------------------------- | | `beforeRowId`     | Identifier of the row next to the row where the control was dropped. An empty string if dropped in the last row or on an empty container.   |  ###### 3. Dropped on an existing empty cell  | Property      | Details                                                                                                          | | ------------- | ---------------------------------------------------------------------------------------------------------------- | | `targetCellId`| Identifier of the cell where the control was dropped |    ###### 4. Dropped on an existing row  | Property          | Details                                                                                                                                     | | ----------------- | ------------------------------------------------------------------------------------------------------------------------------------------- | | `beforeCelllId`   | Identifier of the cell that, after the drop operation, ends located after the dropped control. An empty string if dropped as the last cell. | | `targetRowId`     | Identifier of the row where the control was dropped                                                                                         |
-     */
-    onControlAdded?: (event: CustomEvent) => void;
-    /**
-     * Fired when a control has been removed from the layout  An object containing information of the add operation is sent in the `detail` property of the event object  | Property           | Details                                                     | | ------------------ | ----------------------------------------------------------- | | `controls`         | An array containing the identifiers of the removed controls |
-     */
-    onControlRemoved?: (event: CustomEvent) => void;
-    /**
-     * Fired when the selection has been changed  An object containing information of the select operation is sent in the `detail` property of the event object  | Property       | Details                                                      | | -------------- | ------------------------------------------------------------ | | `controls`     | An array containing the identifiers of the selected controls |
-     */
-    onControlSelected?: (event: CustomEvent) => void;
-    /**
-     * Fired when a control is moved inside the layout editor to a new location  An object containing information of the move operation is sent in the `detail` property of the event object  Regardless where the control was dropped, the detail object will contain information about the source row and the id of the dropped control:  | Property         | Details                                                                                                          | | ---------------- | ---------------------------------------------------------------------------------------------------------------- | | `sourceCellId`   | Identifier of the source cell                                                                                    | | `sourceRowId`    | Identifier of the source row                                                                                     |  Depending on where the control was dropped, additional information will be provided and different properties will be set. There are four possible cases:  1. Dropped on an empty container or on a new row that will be the last row of a container 2. Dropped on a new row of a non empty container 3. Dropped on an existing empty cell 4. Dropped on an existing row   ###### 1. Dropped on an empty container or on a new row that will be the last row of a container  | Property          | Details                                                                                                                                     | | ----------------- | ------------------------------------------------------------------------------------------------------------------------------------------- | | `containerId`     | Identifier of the container where the control was dropped                                                                                   |  ###### 2. Dropped on a new row of a non empty container  | Property          | Details                                                                                                                                     | | ----------------- | ------------------------------------------------------------------------------------------------------------------------------------------- | | `beforeRowId`     | Identifier of the row next to the row where the control was dropped. An empty string if dropped in the last row or on an empty container.   |  ###### 3. Dropped on an existing empty cell  | Property      | Details                                                                                                          | | ------------- | ---------------------------------------------------------------------------------------------------------------- | | `targetCellId`| Identifier of the cell where the control was dropped |    ###### 4. Dropped on an existing row  | Property          | Details                                                                                                                                     | | ----------------- | ------------------------------------------------------------------------------------------------------------------------------------------- | | `beforeCellId`    | Identifier of the cell that, after the drop operation, ends located after the dropped control. An empty string if dropped as the last cell. | | `targetRowId`     | Identifier of the row where the control was dropped                                                                                         |
-     */
-    onMoveCompleted?: (event: CustomEvent) => void;
-    /**
-     * Array with the identifiers of the selected controls. If empty the whole layout-editor is marked as selected.
-     */
-    selectedControls?: string[];
-  }
-
   interface GxLoading {
     /**
      * Sets the caption text.
@@ -1399,8 +1359,6 @@ declare global {
     GxFormField: Components.GxFormField;
     GxGroup: Components.GxGroup;
     GxImage: Components.GxImage;
-    GxLayoutEditorPlaceholder: Components.GxLayoutEditorPlaceholder;
-    GxLayoutEditor: Components.GxLayoutEditor;
     GxLoading: Components.GxLoading;
     GxLottie: Components.GxLottie;
     GxMessage: Components.GxMessage;
@@ -1432,8 +1390,6 @@ declare global {
     "gx-form-field": Components.GxFormFieldAttributes;
     "gx-group": Components.GxGroupAttributes;
     "gx-image": Components.GxImageAttributes;
-    "gx-layout-editor-placeholder": Components.GxLayoutEditorPlaceholderAttributes;
-    "gx-layout-editor": Components.GxLayoutEditorAttributes;
     "gx-loading": Components.GxLoadingAttributes;
     "gx-lottie": Components.GxLottieAttributes;
     "gx-message": Components.GxMessageAttributes;
@@ -1517,22 +1473,6 @@ declare global {
   var HTMLGxImageElement: {
     prototype: HTMLGxImageElement;
     new (): HTMLGxImageElement;
-  };
-
-  interface HTMLGxLayoutEditorPlaceholderElement
-    extends Components.GxLayoutEditorPlaceholder,
-      HTMLStencilElement {}
-  var HTMLGxLayoutEditorPlaceholderElement: {
-    prototype: HTMLGxLayoutEditorPlaceholderElement;
-    new (): HTMLGxLayoutEditorPlaceholderElement;
-  };
-
-  interface HTMLGxLayoutEditorElement
-    extends Components.GxLayoutEditor,
-      HTMLStencilElement {}
-  var HTMLGxLayoutEditorElement: {
-    prototype: HTMLGxLayoutEditorElement;
-    new (): HTMLGxLayoutEditorElement;
   };
 
   interface HTMLGxLoadingElement
@@ -1691,8 +1631,6 @@ declare global {
     "gx-form-field": HTMLGxFormFieldElement;
     "gx-group": HTMLGxGroupElement;
     "gx-image": HTMLGxImageElement;
-    "gx-layout-editor-placeholder": HTMLGxLayoutEditorPlaceholderElement;
-    "gx-layout-editor": HTMLGxLayoutEditorElement;
     "gx-loading": HTMLGxLoadingElement;
     "gx-lottie": HTMLGxLottieElement;
     "gx-message": HTMLGxMessageElement;
@@ -1724,8 +1662,6 @@ declare global {
     "gx-form-field": HTMLGxFormFieldElement;
     "gx-group": HTMLGxGroupElement;
     "gx-image": HTMLGxImageElement;
-    "gx-layout-editor-placeholder": HTMLGxLayoutEditorPlaceholderElement;
-    "gx-layout-editor": HTMLGxLayoutEditorElement;
     "gx-loading": HTMLGxLoadingElement;
     "gx-lottie": HTMLGxLottieElement;
     "gx-message": HTMLGxMessageElement;
--- src/components/layout-editor/layout-editor.spec.ts
+++
@@ -1,8 +0,0 @@
-// import { flush, render } from "@stencil/core/testing";
-import { LayoutEditor } from "./layout-editor";
-
-describe("gx-layout-editor", () => {
-  it("should build", () => {
-    // expect(new LayoutEditor()).toBeTruthy();
-  });
-});
