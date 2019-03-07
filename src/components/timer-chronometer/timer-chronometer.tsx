82fef59501657bf1cb05e0f60702e2904b3c887f
Gonzalo Gallotti <ggallotti@genexus.com>
2019-03-07 16:28:26 -0300

    New Chronometer WebComponent for Chronometer Control Type.

---
+++ src/components/timer-chronometer/timer-chronometer.tsx
@@ -0,0 +1,96 @@
+import {
+  Component,
+  Element,
+  Event,
+  EventEmitter,
+  Method,
+  Prop,
+  State
+} from "@stencil/core";
+import { IComponent } from "../common/interfaces";
+
+@Component({
+  shadow: false,
+  styleUrl: "chronometer.scss",
+  tag: "gx-chronometer"
+})
+export class TimerChronometer implements IComponent {
+  @Element() element;
+
+  timer: number;
+  eventTimer: number;
+  startedTime: number;
+  started: boolean;
+
+  @State() currentTime: number;
+
+  /**
+   * Defines the initial Chronometer value in milliseconds
+   */
+  @Prop() initialMilliseconds = 0;
+
+  /**
+   * Defines the interval in milliseconds that the function onTick will be called.
+   */
+  @Prop() tickInterval = 1000;
+
+  /**
+   * When the chronnometer reach the maxValue (in milliseconds),
+   * MaxValueText will be shown instead of the Chronometer.
+   */
+  @Prop() maxValue = 0;
+  @Prop() maxValueText: string;
+
+  @Event() tick: EventEmitter;
+
+  tickHandler() {
+    if (this.tick) {
+      this.tick.emit();
+    }
+  }
+
+  componentDidUnload() {
+    this.stop();
+  }
+
+  @Method()
+  start() {
+    this.stop();
+    this.started = true;
+    this.currentTime = Date.now();
+    this.startedTime = this.currentTime - this.initialMilliseconds;
+
+    this.timer = window.setInterval(() => {
+      this.currentTime = Date.now();
+    }, 1000);
+
+    this.eventTimer = window.setInterval(() => {
+      this.tickHandler();
+    }, this.tickInterval);
+  }
+
+  @Method()
+  stop() {
+    window.clearInterval(this.eventTimer);
+    window.clearInterval(this.timer);
+    this.started = false;
+  }
+  @Method()
+  reset() {
+    this.start();
+  }
+
+  render() {
+    if (!this.started) {
+      return;
+    }
+
+    const timeMS = this.currentTime - this.startedTime;
+    const time = Math.floor(timeMS / 1000);
+    if (timeMS < this.maxValue || this.maxValue === 0) {
+      return <span>{time}</span>;
+    } else {
+      return <span>{this.maxValueText}</span>;
+    }
+  }
+}
