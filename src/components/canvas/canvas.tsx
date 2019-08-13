c59abc567a7d403a252124a3918b1142d3c75f6c
Marcos Á. Medina <33690842+MarkMed@users.noreply.github.com>
2019-08-13 10:16:03 -0300

    ISwipeable interface added to canvas to test it

--- src/components/canvas/canvas.tsx
+++ src/components/canvas/canvas.tsx
@@ -5,6 +5,7 @@ import {
   IDisableableComponent,
   IVisibilityComponent
 } from "../common/interfaces";
+import { ISwipeable, makeSwipeable } from "../common/swipeable";
 
 @Component({
   shadow: false,
@@ -16,6 +17,7 @@ export class Canvas
     IComponent,
     IVisibilityComponent,
     IDisableableComponent,
+    ISwipeable,
     IClickableComponent {
   @Element() element: HTMLElement;
 
@@ -42,12 +44,37 @@ export class Canvas
   @Event() onClick: EventEmitter;
   // TODO: Implement touch devices events (Tap, DoubleTap, LongTap, SwipeX)
 
+  /**
+   * Emitted when the element is swiped.
+   */
+  @Event() onSwipe: EventEmitter;
+  /**
+   * Emitted when the element is swiped in upward direction.
+   */
+  @Event() onSwipeUp: EventEmitter;
+  /**
+   * Emitted when the element is swiped right direction.
+   */
+  @Event() onSwipeRight: EventEmitter;
+  /**
+   * Emitted when the element is swiped downward direction.
+   */
+  @Event() onSwipeDown: EventEmitter;
+  /**
+   * Emitted when the element is swiped left direction..
+   */
+  @Event() onSwipeLeft: EventEmitter;
+
   handleClick(event: UIEvent) {
     if (this.disabled) {
       return;
     }
 
     this.onClick.emit(event);
+  }
+
+  componentDidLoad() {
+    makeSwipeable(this);
   }
 
   render() {
