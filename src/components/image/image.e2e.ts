9bf1c8522d0be43fca6901bca161c0a5b2ac03a0
Nicolas Cardelino <ncardeli@gmail.com>
2019-12-11 16:36:58 -0300

    Fix scrollbars shown when lazy loading (#85)

--- src/components/image/image.e2e.ts
+++ src/components/image/image.e2e.ts
@@ -13,18 +13,19 @@ describe("gx-image", () => {
   });
 
   it("inner img should have alternate text", async () => {
-    const img = await page.find("img");
+    const img = await element.find("img");
     expect(await img.getProperty("alt")).toEqual(
       await element.getProperty("alt")
     );
   });
 
   it("should lazy load the image", async () => {
-    const img = await page.find("img");
+    const img = await element.find("img");
     const className: string = await img.getProperty("className");
     expect(className.includes("gx-lazyload")).toBe(true);
     expect(await img.getAttribute("src")).toBeNull();
     expect(await img.getAttribute("data-src")).toBe("img.png");
+    expect(await element.classList.contains("gx-img-lazyloading")).toBe(true);
   });
 
   it("should load the image", async () => {
@@ -34,6 +35,7 @@ describe("gx-image", () => {
     const className: string = await img.getProperty("className");
     expect(className.includes("gx-lazyload")).toBe(false);
     expect(await img.getAttribute("src")).toBe("img.png");
+    expect(await element.classList.contains("gx-img-lazyloading")).toBe(false);
   });
 
   it("should set the inner image class", async () => {
