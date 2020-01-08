c5c7618db3aac74f529e0f1ecebac1c8f31e151b
Nicolas Cardelino <ncardeli@gmail.com>
2020-01-08 14:25:49 -0300

    Add autoGrow property to gx-image component (#93)
    * Add autoGrow property to gx-image component
    
    * Fix Component interface definition
    
    * Fix lazy loading failing tests
    
    * Ignore test data file failing lint

--- src/components/image/image.e2e.ts
+++ src/components/image/image.e2e.ts
@@ -19,31 +19,19 @@ describe("gx-image", () => {
     );
   });
 
-  it("should lazy load the image", async () => {
-    const img = await element.find("img");
-    const className: string = await img.getProperty("className");
-    expect(className.includes("gx-lazyload")).toBe(true);
-    expect(await img.getAttribute("src")).toBeNull();
-    expect(await img.getAttribute("data-src")).toBe("img.png");
-    expect(await element.classList.contains("gx-img-lazyloading")).toBe(true);
-  });
-
-  it("should load the image", async () => {
-    await element.setAttribute("lazy-load", false);
-    await page.waitForChanges();
-    const img = await page.find("img");
-    const className: string = await img.getProperty("className");
-    expect(className.includes("gx-lazyload")).toBe(false);
-    expect(await img.getAttribute("src")).toBe("img.png");
-    expect(await element.classList.contains("gx-img-lazyloading")).toBe(false);
-  });
-
   it("should set the inner image class", async () => {
     await element.setAttribute("css-class", "danger");
     await page.waitForChanges();
     const img = await page.find("img");
     const className: string = await img.getProperty("className");
     expect(className.includes("danger")).toBe(true);
+  });
+
+  it("should add gx-img-no-auto-grow class", async () => {
+    expect(element.classList.contains("gx-img-no-auto-grow")).toBe(false);
+    await element.setProperty("autoGrow", false);
+    await page.waitForChanges();
+    expect(element.classList.contains("gx-img-no-auto-grow")).toBe(true);
   });
 
   it("should fire click event", async () => {
@@ -62,5 +50,37 @@ describe("gx-image", () => {
     isHidden = await element.getProperty("hidden");
 
     expect(isHidden).toBe(true);
+  });
+});
+
+describe("gx-image lazy loading", () => {
+  let element: E2EElement;
+  let page: E2EPage;
+
+  beforeEach(async () => {
+    page = await newE2EPage();
+    await page.setContent(
+      "<gx-image src='img.png' alt='Alternate text' style='position: absolute; top: -3000px; left: -3000px;'></gx-image>"
+    );
+    element = await page.find("gx-image");
+  });
+
+  it("should load the image", async () => {
+    await element.setAttribute("lazy-load", false);
+    await page.waitForChanges();
+    const img = await page.find("img");
+    const className: string = await img.getProperty("className");
+    expect(className.includes("gx-lazyload")).toBe(false);
+    expect(await img.getAttribute("src")).toBe("img.png");
+    expect(await element.classList.contains("gx-img-lazyloading")).toBe(false);
+  });
+
+  it("should lazy load the image", async () => {
+    const img = await element.find("img");
+    const className: string = await img.getProperty("className");
+    expect(className.includes("gx-lazyload")).toBe(true);
+    expect(await img.getAttribute("src")).toBeNull();
+    expect(await img.getAttribute("data-src")).toBe("img.png");
+    expect(await element.classList.contains("gx-img-lazyloading")).toBe(true);
   });
 });
