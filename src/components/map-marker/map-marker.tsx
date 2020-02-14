4584908a68393f8bce63f6c532cb08bb5ac8f33a
Marcos Á. Medina <33690842+MarkMed@users.noreply.github.com>
2020-02-14 10:45:24 -0300

    Comments of ToDo and comment of ignore lint has been deleted.

--- src/components/map-marker/map-marker.tsx
+++ src/components/map-marker/map-marker.tsx
@@ -155,7 +155,6 @@ export class MapMarker implements GxComponent {
     this.setPopup();
   }
 
-  // eslint-disable-next-line @stencil/own-methods-must-be-private
   componentDidUnload() {
     this.gxMapMarkerDeleted.emit(this.markerInstance);
   }
