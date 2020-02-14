7c8ba39557431c6bcc6529f9f50ed2d93ccaeed1
Marcos Á. Medina <33690842+MarkMed@users.noreply.github.com>
2020-02-14 10:19:34 -0300

    Popup sizes now depends of map sizes. No fixed values.

--- src/components/map-marker/map-marker.tsx
+++ src/components/map-marker/map-marker.tsx
@@ -155,6 +155,7 @@ export class MapMarker implements GxComponent {
     this.setPopup();
   }
 
+  // eslint-disable-next-line @stencil/own-methods-must-be-private
   componentDidUnload() {
     this.gxMapMarkerDeleted.emit(this.markerInstance);
   }
