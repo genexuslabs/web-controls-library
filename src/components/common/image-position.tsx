40c14b81cfaef0e3023056b84cb47d403389d9b0
Nicolás Cardelino <ncardeli@gmail.com>
2020-03-13 17:23:17 -0300

    Add default image position

--- src/components/common/image-position.tsx
+++ src/components/common/image-position.tsx
@@ -8,10 +8,11 @@ export function imagePositionRender(slots: {
   return [slots.mainImage, slots.disabledImage, <span>{slots.default}</span>];
 }
 
+const DEFAULT_IMAGE_POSITION = "above";
 export function imagePositionClass(
-  imagePosition: "above" | "before" | "after" | "below" | "behind" = "above"
+  imagePosition: "above" | "before" | "after" | "below" | "behind"
 ) {
-  return `gx-image-position--${imagePosition}`;
+  return `gx-image-position--${imagePosition || DEFAULT_IMAGE_POSITION}`;
 }
 
 export const hideMainImageWhenDisabledClass = "gx-image-position--hide-main";
