4d87201fea481ad247a11c350ff004f18e7ba4b5
Marcos Á. Medina <marcmedi1@hotmail.com>
2020-06-13 01:24:06 -0300

    some tests for the new changes

--- src/components/interactive-image/interactive-image.e2e.ts
+++ src/components/interactive-image/interactive-image.e2e.ts
@@ -6,10 +6,24 @@ describe("gx-interactive-image", () => {
   beforeEach(async () => {
     page = await newE2EPage();
     await page.setContent("<gx-interactive-image></gx-interactive-image>");
+    await page.waitForChanges();
     element = await page.find("gx-interactive-image");
   });
 
   it("should properly render and locate the component", async () => {
-    expect(element.textContent).toEqual("interactiveImage");
+    expect(await element.find("img")).toBeTruthy;
+  });
+
+  it("should set the src", async () => {
+    element.setAttribute(
+      "src",
+      "https://www.genexus.com/media/images/genexus-share-link-image.png"
+    );
+    await page.waitForChanges();
+
+    const imgElement = await element.find("img");
+    expect(imgElement.getAttribute("src")).toEqual(
+      "https://www.genexus.com/media/images/genexus-share-link-image.png"
+    );
   });
 });
