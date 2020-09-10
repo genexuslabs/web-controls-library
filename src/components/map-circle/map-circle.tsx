8f256897c2afdbedcf0d5e69fa12f9572c668c9b
fedes1987 <fsalomon@genexus.com>
2020-09-10 19:06:19 -0300

    Add gx-map-circle files

---
+++ src/components/map-circle/map-circle.tsx
@@ -0,0 +1,77 @@
+import {
+  Component,
+  Element,
+  Event,
+  EventEmitter,
+  h,
+  Prop
+} from "@stencil/core";
+import { Component as GxComponent } from "../common/interfaces";
+import { circle } from "leaflet/dist/leaflet-src.esm";
+import { parseCoords } from "../common/coordsValidate";
+
+@Component({
+  shadow: false,
+  tag: "gx-map-circle"
+})
+export class MapCircle implements GxComponent {
+  @Element() element: HTMLGxMapCircleElement;
+  private circleInstance: any;
+
+  /**
+   * The coordinates where the circle will appear in the map.
+   *
+   */
+  @Prop({ mutable: true }) coords = "0, 0";
+
+  /**
+   * The radius that the circle will have in the map. It's expressed in meters.
+   *
+   */
+  @Prop({ mutable: true }) radius = 1000;
+
+  /**
+   * Emmits when the element is added to a `<gx-map>`.
+   *
+   */
+  @Event() gxMapCircleDidLoad: EventEmitter;
+
+  /**
+   * Emmits when the element is deleted from a `<gx-map>`.
+   *
+   */
+  @Event() gxMapCircleDeleted: EventEmitter;
+
+  private setupCircle(coords) {
+    this.circleInstance = circle(coords, this.radius, {
+      color: "red",
+      weight: 3
+    });
+  }
+
+  componentDidLoad() {
+    const coords = parseCoords(this.coords);
+    if (coords !== null) {
+      this.setupCircle(coords);
+    } else {
+      console.warn(
+        "GX warning: Can not read 'coords' attribute, default coords set (gx-map-circle)",
+        this.element
+      );
+      this.setupCircle([0, 0]);
+    }
+    this.gxMapCircleDidLoad.emit(this.circleInstance);
+  }
+
+  componentDidUnload() {
+    this.gxMapCircleDeleted.emit(this.circleInstance);
+  }
+
+  render() {
+    return (
+      <div class="popup-data-container">
+        <slot />
+      </div>
+    );
+  }
+}
