33b256a608952ea771ff9cc811f6cc9ac59f1d04
fedes1987 <fsalomon@genexus.com>
2020-09-11 19:36:04 -0300

    Extract parseCoords logic to new fnc parseGeoJson

--- src/components/common/coordsValidate.ts
+++ src/components/common/coordsValidate.ts
@@ -4,39 +4,42 @@ export function parseCoords(coord): string[] {
   if (result !== null) {
     return result.slice(1);
   } else {
-    //it's geopoint/geostring type
-    let geoObject;
-    try {
-      geoObject = JSON.parse(coord);
-    } catch (e) {
-      throw new Error(e);
-    }
-    const hasType = geoObject.hasOwnProperty("type");
-    if (hasType) {
-      switch (geoObject.type) {
-        case "Point":
-        case "MultiPoint":
-        case "LineString":
-        case "MultiLineString":
-        case "Polygon":
-        case "MultiPolygon":
-          return geoObject.coordinates;
-        case "FeatureCollection":
-          return geoObject.features;
-      }
-    } else {
-      const hasGeography = geoObject.hasOwnProperty("Geography");
-      if (hasGeography) {
-        return geoObject;
-      } else {
-        const isArray =
-          geoObject[0].hasOwnProperty("Geography") && Array.isArray(geoObject);
-        if (isArray) {
-          geoObject.IsLayersArray = true;
-          return geoObject;
-        }
-      }
-    }
-    return geoObject;
+    return parseGeoJson(coord);
   }
+}
+
+function parseGeoJson(coord) {
+  let geoObject;
+  try {
+    geoObject = JSON.parse(coord);
+  } catch (e) {
+    throw new Error(e);
+  }
+  const hasType = geoObject.hasOwnProperty("type");
+  if (hasType) {
+    switch (geoObject.type) {
+      case "Point":
+      case "MultiPoint":
+      case "LineString":
+      case "MultiLineString":
+      case "Polygon":
+      case "MultiPolygon":
+        return geoObject.coordinates;
+      case "FeatureCollection":
+        return geoObject.features;
+    }
+  } else {
+    const hasGeography = geoObject.hasOwnProperty("Geography");
+    if (hasGeography) {
+      return geoObject;
+    } else {
+      const isArray =
+        geoObject[0].hasOwnProperty("Geography") && Array.isArray(geoObject);
+      if (isArray) {
+        geoObject.IsLayersArray = true;
+        return geoObject;
+      }
+    }
+  }
+  return geoObject;
 }
