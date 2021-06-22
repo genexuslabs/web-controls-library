41c7dc5071119d4f0f692d7a269bffd06c627825
Nicolás Cámera <nico_camera98@hotmail.com>
2021-06-22 15:22:32 -0300

    Various changes
       - Ordered css to match .tsx layout
       - Minor fixes in the behavior when the src property is set at the beginning
       - The clickImageAction event was fixed
       - Now, the change image button is displayed above the image and in the bottom right position.

--- src/components/image-upload/image-upload.tsx
+++ src/components/image-upload/image-upload.tsx
@@ -79,9 +79,9 @@ export class ImageUpload implements GxComponent {
     event.stopPropagation();
   }
 
-  private clickImageAction(event: MouseEvent) {
+  private clickImageAction = (event: MouseEvent) => {
     this.click.emit(event);
-  }
+  };
 
   // If there is no image, this directly opens the File System to select an image.
   // In othercase, this allows to change or remove the image
@@ -155,7 +155,13 @@ export class ImageUpload implements GxComponent {
   // SVG used to print the search image button
   private getSearchPlusSolidSVG(): any {
     return (
-      <svg viewBox="0 0 27 27" class="svgSearch">
+      <svg
+        viewBox="0 0 27 27"
+        class={{
+          svgSearch: true,
+          disabled: this.src !== ""
+        }}
+      >
         <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
         <path d="M0 0h24v24H0z" fill="none" />
       </svg>
@@ -165,7 +171,13 @@ export class ImageUpload implements GxComponent {
   // SVG used to print the change image button
   private getPencilAltSolidSVG(): any {
     return (
-      <svg viewBox="-105 0 750 640" class="svgPencil disabled">
+      <svg
+        viewBox="-105 0 750 640"
+        class={{
+          svgPencil: true,
+          disabled: this.src === ""
+        }}
+      >
         <path
           fill="black"
           d="M497.9 142.1l-46.1 46.1c-4.7 4.7-12.3 4.7-17 0l-111-111c-4.7-4.7-4.7-12.3 0-17l46.1-46.1c18.7-18.7 49.1-18.7 67.9 0l60.1 60.1c18.8 18.7 18.8 49.1 0 67.9zM284.2 99.8L21.6 362.4.4 483.9c-2.9 16.4 11.4 30.6 27.8 27.8l121.5-21.3 262.6-262.6c4.7-4.7 4.7-12.3 0-17l-111-111c-4.8-4.7-12.4-4.7-17.1 0zM124.1 339.9c-5.5-5.5-5.5-14.3 0-19.8l154-154c5.5-5.5 14.3-5.5 19.8 0s5.5 14.3 0 19.8l-154 154c-5.5 5.5-14.3 5.5-19.8 0zM88 424h48v36.3l-64.5 11.3-31.1-31.1L51.7 376H88v48z"
@@ -184,29 +196,21 @@ export class ImageUpload implements GxComponent {
               alt={this.alt}
               disabled={this.disabled}
               onClick={this.clickImageAction}
-              height={`${this.element.parentElement.getBoundingClientRect()
-                .height - 30}px`}
             ></gx-image>
-            <div class="button-edit-container">
-              <button
-                class={{
-                  "image-edit": true,
-                  disabled: this.readonly
-                }}
-                disabled={this.disabled}
-                onClick={this.triggerAction}
-              >
-                {this.getSearchPlusSolidSVG()}
-                {this.getPencilAltSolidSVG()}
-              </button>
-            </div>
+            {!this.readonly && (
+              <div class="button-edit-container">
+                <button
+                  class="image-edit"
+                  disabled={this.disabled}
+                  onClick={this.triggerAction}
+                >
+                  {this.getSearchPlusSolidSVG()}
+                  {this.getPencilAltSolidSVG()}
+                </button>
+              </div>
+            )}
           </div>
-          <gx-modal
-            // onClose={
-            // this.closeAction // What's the purpose of using this?
-            // }
-            class="action-dialog"
-          >
+          <gx-modal class="action-dialog">
             <div slot="header">
               {this.modalTitle === null ? document.title : this.modalTitle}
             </div>
