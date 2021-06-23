5149071b3bd193de08b83b50b49ef7fcfa59d0ae
Nicolás Cámera <nico_camera98@hotmail.com>
2021-06-23 15:23:13 -0300

    Simplified the way that edit button image changes

--- src/components/image-upload/image-upload.tsx
+++ src/components/image-upload/image-upload.tsx
@@ -148,12 +148,6 @@ export class ImageUpload implements GxComponent {
   };
 
   private clearImageAction = () => {
-    const svgSearch = this.element.querySelector(".svgSearch");
-    const svgPencil = this.element.querySelector(".svgPencil");
-
-    svgSearch.setAttribute("class", "svgSearch");
-    svgPencil.setAttribute("class", "svgPencil disabled");
-
     this.element.querySelector("input").value = "";
     this.element.setAttribute("src", "");
     this.element.setAttribute("alt", "");
@@ -183,15 +177,6 @@ export class ImageUpload implements GxComponent {
     const file = elem.querySelector("input").files[0];
     const alt = this.getFileNameWithoutExtension(file.name);
 
-    // If there was no image, switch to edit button
-    if (this.src === "") {
-      const svgSearch = this.element.querySelector(".svgSearch");
-      const svgPencil = this.element.querySelector(".svgPencil");
-
-      svgSearch.setAttribute("class", "svgSearch disabled");
-      svgPencil.setAttribute("class", "svgPencil");
-    }
-
     this.reader.addEventListener(
       "load",
       function() {
@@ -209,13 +194,7 @@ export class ImageUpload implements GxComponent {
   // SVG used to print the search image button
   private getSearchPlusSolidSVG(): any {
     return (
-      <svg
-        viewBox="3 2 20 20"
-        class={{
-          svgSearch: true,
-          disabled: this.src !== ""
-        }}
-      >
+      <svg viewBox="3 2 20 20">
         <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
         <path d="M0 0h24v24H0z" fill="none" />
       </svg>
@@ -225,13 +204,7 @@ export class ImageUpload implements GxComponent {
   // SVG used to print the change image button
   private getPencilAltSolidSVG(): any {
     return (
-      <svg
-        viewBox="0 -35 570 570"
-        class={{
-          svgPencil: true,
-          disabled: this.src === ""
-        }}
-      >
+      <svg viewBox="0 -35 570 570">
         <path
           fill="black"
           d="M497.9 142.1l-46.1 46.1c-4.7 4.7-12.3 4.7-17 0l-111-111c-4.7-4.7-4.7-12.3 0-17l46.1-46.1c18.7-18.7 49.1-18.7 67.9 0l60.1 60.1c18.8 18.7 18.8 49.1 0 67.9zM284.2 99.8L21.6 362.4.4 483.9c-2.9 16.4 11.4 30.6 27.8 27.8l121.5-21.3 262.6-262.6c4.7-4.7 4.7-12.3 0-17l-111-111c-4.8-4.7-12.4-4.7-17.1 0zM124.1 339.9c-5.5-5.5-5.5-14.3 0-19.8l154-154c5.5-5.5 14.3-5.5 19.8 0s5.5 14.3 0 19.8l-154 154c-5.5 5.5-14.3 5.5-19.8 0zM88 424h48v36.3l-64.5 11.3-31.1-31.1L51.7 376H88v48z"
@@ -272,8 +245,9 @@ export class ImageUpload implements GxComponent {
                     disabled={this.disabled}
                     onClick={this.triggerAction}
                   >
-                    {this.getSearchPlusSolidSVG()}
-                    {this.getPencilAltSolidSVG()}
+                    {this.src === ""
+                      ? this.getSearchPlusSolidSVG()
+                      : this.getPencilAltSolidSVG()}
                   </button>
                 </div>
               )}
