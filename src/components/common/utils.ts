40aba676d6647c46fe1f1d8b4c9ac885c63cdc3c
Nicolás Cámera <nico_camera98@hotmail.com>
2021-12-27 17:20:25 -0300

    Add `getFileNameWithoutExtension()` function

--- src/components/common/utils.ts
+++ src/components/common/utils.ts
@@ -45,3 +45,25 @@ export function overrideMethod(
     }
   };
 }
+
+export function getFileNameWithoutExtension(filePath: string) {
+  let lastSlashIndex = filePath.lastIndexOf("/");
+
+  // If the function is call in the same folder of the file
+  if (lastSlashIndex === -1) {
+    lastSlashIndex = 0;
+  }
+
+  // We store the fileName that could have extension (+1 removes the last slash)
+  const fileName = filePath.substring(lastSlashIndex + 1);
+
+  const extensionIndex = fileName.lastIndexOf(".");
+
+  // If the file does not have extension
+  if (extensionIndex === -1) {
+    return fileName;
+  }
+
+  // Returns the name between the last "/" and the last "." of the `fileName`
+  return fileName.substring(0, extensionIndex);
+}
