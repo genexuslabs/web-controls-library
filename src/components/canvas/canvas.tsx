7ac4f358ff26eafdae11761cc3a3f53da56b5d1f
ncamera <81186472+ncamera@users.noreply.github.com>
2022-02-14 14:36:46 -0300

    Implementation of autogrow in `gx-canvas` control when cells overflows
     - We use ResizeObservers to monitor size changes in the gx-canvas-cell controls
    
     - Every time the ResizeObserver interrupts, we check if there is a cell that has more height than the gx-canvas control. Otherwise, we check if the tallest gx-canvas-cell decreased its height when (in this case, we have to find a new "tallest" gx-canvas-cell to correct the height of the gx-canvas)
    
     - If the gx-canvas control never had a cell that exceeded its height, then: this.canvasFixedHeight == null.
    
     - When this.canvasFixedHeight turns stops being null, we fix the relative values of top, min-height and max-height in all the gx-canvas-cell.

--- src/components/canvas/canvas.tsx
+++ src/components/canvas/canvas.tsx
@@ -5,7 +5,9 @@ import {
   EventEmitter,
   Prop,
   h,
-  Host
+  Host,
+  State,
+  Method
 } from "@stencil/core";
 import {
   ClickableComponent,
@@ -14,6 +16,21 @@ import {
   VisibilityComponent
 } from "../common/interfaces";
 import { Swipeable, makeSwipeable } from "../common/swipeable";
+
+const THRESHOLD = 1.75;
+
+/* - - - - - - - - SELECTORS - - - - - - - - */
+const ALL_CANVAS_CELLS = ":scope > gx-canvas-cell";
+
+const AUTOGROW_CANVAS_CELLS = ":scope > .auto-grow-cell";
+
+const AUTOGROW_CANVAS_CELL_CLASS = (id: string) => {
+  return `autogrow-cell-${id}`;
+};
+
+const AUTOGROW_CANVAS_CELL_BY_ID = (id: string) => {
+  return `:scope > .${AUTOGROW_CANVAS_CELL_CLASS(id)}`;
+};
 
 @Component({
   shadow: false,
@@ -92,7 +109,86 @@ export class Canvas
    */
   @Event() swipeLeft: EventEmitter;
 
-  private watchForItemsObserver: MutationObserver;
+  /**
+   * Its value is not null when there is a `gx-canvas-cell` that has more
+   * height than the `gx-canvas` control. In this case, this state takes the
+   * value of the highest `gx-canvas-cell`.
+   */
+  @State() canvasFixedHeight: number = null;
+
+  /*  Used to optimize height adjustments. This variable allows us to ignore
+      the height adjustments triggered by a gx-canvas-cell that had less height
+      than the highest gx-canvas-cell.
+  */
+  private highestCanvasCellId: string = null;
+
+  // Observers
+  private watchForItemsObserver: ResizeObserver;
+
+  /**
+   *
+   */
+  @Method()
+  async setObserver() {
+    this.setCanvasCellsObserver();
+  }
+
+  /*  Observes each gx-canvas-cell that has auto-grow = True. When interrupt,
+      it checks if is needed to update the canvas height
+  */
+  private setCanvasCellsObserver() {
+    this.watchForItemsObserver = new ResizeObserver(entries => {
+      let maxCanvasCellId: string;
+      let maxCanvasCellHeight = 0;
+
+      /*  If more than one gx-canvas-cell interrupted, we search for the cell
+          with the maximum height
+      */
+      entries.forEach(entry => {
+        const canvasCellId = entry.target.id;
+
+        const canvasCell: HTMLGxCanvasCellElement = this.element.querySelector(
+          AUTOGROW_CANVAS_CELL_BY_ID(canvasCellId)
+        );
+        const canvasCellHeight = this.getCanvasCellTotalHeight(canvasCell);
+
+        if (maxCanvasCellHeight < canvasCellHeight) {
+          maxCanvasCellHeight = canvasCellHeight;
+          maxCanvasCellId = canvasCellId;
+        }
+      });
+
+      /*  The gx-canvas-cell that represent the max height constraint is used
+          to adjust the gx-canvas height
+       */
+      this.adjustCanvasHeight(maxCanvasCellId, maxCanvasCellHeight);
+    });
+
+    /*  ResizeObserver ONLY for:
+          - gx-canvas-cell with auto-grow = True
+    */
+    const autoGrowCanvasCells = this.element.querySelectorAll(
+      AUTOGROW_CANVAS_CELLS
+    );
+    autoGrowCanvasCells.forEach((canvasCell: HTMLGxCanvasCellElement, id) => {
+      canvasCell.id = id.toString();
+      canvasCell.classList.add(AUTOGROW_CANVAS_CELL_CLASS(id.toString()));
+
+      this.watchForItemsObserver.observe(canvasCell);
+    });
+  }
+
+  private fixCanvasHeight(maxHeightConstraint: number) {
+    /*  In each gx-canvas-cell, we adapt its top, min-height and max-height
+        values to make them absolute
+    */
+    const canvasCells = this.element.querySelectorAll(ALL_CANVAS_CELLS);
+    canvasCells.forEach((canvasCell: HTMLGxCanvasCellElement) => {
+      this.fixCanvasCellProperties(canvasCell);
+    });
+
+    this.canvasFixedHeight = maxHeightConstraint;
+  }
 
   private handleClick(event: UIEvent) {
     if (this.disabled) {
@@ -102,8 +198,108 @@ export class Canvas
     this.gxClick.emit(event);
   }
 
+  private getCanvasCellTotalHeight(
+    canvasCell: HTMLGxCanvasCellElement
+  ): number {
+    return canvasCell.clientHeight + canvasCell.offsetTop;
+  }
+
+  /*  This functions is called the first time that
+        this.canvasFixedHeight != null
+  
+      Due to at this point we have to resize the gx-canvas, we fix the relative
+      values of top, min-height and max-height in all the gx-canvas-cell
+  */
+  private fixCanvasCellProperties(canvasCell: HTMLGxCanvasCellElement) {
+    canvasCell.style.setProperty("top", `${canvasCell.offsetTop}px`);
+
+    const minHeight = canvasCell.minHeight;
+    let fixedHeight;
+
+    const canvasInitialHeight = this.element.clientHeight;
+
+    // Mixed height. For example, "min-height: calc( 50% + -75px )"
+    if (minHeight.includes("calc")) {
+      const reRelative = /-?\d+%/g;
+      const reAbsolute = /-?\d+px/g;
+
+      const relative = Number(minHeight.match(reRelative)[0].replace("%", ""));
+      const absolute = Number(minHeight.match(reAbsolute)[0].replace("px", ""));
+
+      fixedHeight = `${absolute + canvasInitialHeight * (relative / 100)}px`;
+
+      // Relative height. For example, "min-height: 50%"
+    } else if (minHeight.includes("%")) {
+      const relative = Number(minHeight.replace("%", "").trim());
+
+      fixedHeight = `${canvasInitialHeight * (relative / 100)}px`;
+
+      // Absolute height. For example, "min-height: 100px"
+    } else {
+      fixedHeight = minHeight;
+    }
+
+    canvasCell.style.setProperty("min-height", fixedHeight);
+
+    // If the gx-canvas-cell has auto-grow == False, we limit its height
+    if (canvasCell.maxHeight != null) {
+      canvasCell.style.setProperty("max-height", fixedHeight);
+    }
+  }
+
   componentDidLoad() {
     makeSwipeable(this);
+  }
+
+  /*  The height adjustment of the gx-canvas will trigger if one of the
+      following conditions are true:
+        - A gx-canvas-cell has more height than the gx-canvas.
+
+        - The previous gx-canvas-cell that had the max-height and caused the
+          gx-canvas to stretch, decreased now its height.
+  */
+  private adjustCanvasHeight(canvasCellId: string, canvasCellHeight: number) {
+    if (this.canvasFixedHeight == null) {
+      if (this.element.clientHeight + THRESHOLD < canvasCellHeight) {
+        this.fixCanvasHeight(canvasCellHeight);
+      }
+
+      return;
+    }
+
+    // If the gx-canvas-cell provokes overflow-y
+    if (this.canvasFixedHeight <= canvasCellHeight) {
+      this.canvasFixedHeight = canvasCellHeight;
+      this.highestCanvasCellId = canvasCellId;
+
+      // If the gx-canvas-cell with the max-height decreased its height
+    } else if (this.highestCanvasCellId === canvasCellId) {
+      this.canvasFixedHeight = this.getMaxHeightConstraint();
+    }
+  }
+
+  /*  Returns the maxHeight of all gx-canvas-cell that has auto-grow == True.
+      Also, stores the id of the gx-canvas-cell that acomplish the previous
+      condition
+  */
+  private getMaxHeightConstraint(): number {
+    const autoGrowCanvasCells = this.element.querySelectorAll(
+      AUTOGROW_CANVAS_CELLS
+    );
+
+    let maxCanvasCellHeight = 0;
+
+    // id starts on 0
+    autoGrowCanvasCells.forEach((canvasCell: HTMLGxCanvasCellElement, id) => {
+      const canvasCellHeight = this.getCanvasCellTotalHeight(canvasCell);
+
+      if (maxCanvasCellHeight < canvasCellHeight) {
+        maxCanvasCellHeight = canvasCellHeight;
+        this.highestCanvasCellId = id.toString();
+      }
+    });
+
+    return maxCanvasCellHeight;
   }
 
   disconnectedCallback() {
@@ -117,7 +313,16 @@ export class Canvas
     this.element.addEventListener("click", this.handleClick);
 
     return (
-      <Host>
+      <Host
+        style={{
+          width: this.width,
+          height:
+            this.canvasFixedHeight == null
+              ? this.minHeight
+              : `${this.canvasFixedHeight}px`,
+          "min-height": this.minHeight
+        }}
+      >
         <slot />
       </Host>
     );
