74bbcfbc53ef529eb98591df6c3c06d9c7b02acc
Nicolás Cámera <nico_camera98@hotmail.com>
2022-05-23 16:59:44 -0300

    Add attachMouseHorizontalScrollHandler function
    Also, fix dispatching event click listeners when the gx-grid-smart-css content is dragged (direction="horizontal").
    
    issue: 96365

--- src/components/common/utils.ts
+++ src/components/common/utils.ts
@@ -70,3 +70,73 @@ export function getFileNameWithoutExtension(filePath: string) {
   // Returns the name between the last "/" and the last "." of the `fileName`
   return fileName.substring(0, extensionIndex);
 }
+
+/**
+ * Implement horizontal scrolling by dragging the `scrollableContainer` element
+ * @param scrollableContainer Draggable element
+ */
+export function attachMouseHorizontalScrollHandler(
+  scrollableContainer: HTMLElement
+) {
+  // If we are on a mobile device
+  if (window.matchMedia("(pointer: coarse)").matches) {
+    return;
+  }
+  const SCROLL_SPEED = 1;
+
+  let isMouseDown = false;
+  let scrollableContainerHasBeenDragged = false;
+
+  /** Relative to the left edge of the entire document */
+  let initialXPosition;
+  let initialScrollLeftPosition;
+
+  scrollableContainer.addEventListener("mousedown", (event: MouseEvent) => {
+    // Reset variable as in some scenarios it might be true
+    scrollableContainerHasBeenDragged = false;
+
+    isMouseDown = true;
+    initialXPosition = event.pageX;
+    initialScrollLeftPosition = scrollableContainer.scrollLeft;
+  });
+
+  scrollableContainer.addEventListener("mousemove", (event: MouseEvent) => {
+    event.preventDefault();
+    if (!isMouseDown) {
+      return;
+    }
+    scrollableContainerHasBeenDragged = true;
+
+    const currentXPosition = event.pageX;
+    const walk = (currentXPosition - initialXPosition) * SCROLL_SPEED;
+    scrollableContainer.scrollLeft = initialScrollLeftPosition - walk;
+  });
+
+  /*
+   * Since the click event fires when a mousedown and mouseup event occur on
+   * the same element, we need to prevent event listeners from being dispatched
+   * in the capture phase if the `scrollableContainer` has been dragged
+   */
+  scrollableContainer.addEventListener(
+    "click",
+    event => {
+      if (!scrollableContainerHasBeenDragged) {
+        return;
+      }
+      scrollableContainerHasBeenDragged = false;
+
+      // Stop event listener
+      event.stopImmediatePropagation();
+      event.stopPropagation();
+    },
+    true
+  );
+
+  const stopDragging = () => {
+    isMouseDown = false;
+  };
+
+  // Stop dragging events
+  scrollableContainer.addEventListener("mouseleave", stopDragging);
+  scrollableContainer.addEventListener("mouseup", stopDragging);
+}
