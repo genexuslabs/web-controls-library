bf06026aedc6a1f19301b194d884bb4ef5692453
Nicolás Cámera <nico_camera98@hotmail.com>
2022-07-28 15:18:51 -0300

    Add new design for gx-image-picker's modal
     - Moved private variables to the top of the class.
    
     - The modal's buttons now have svg icons.

--- src/components/image-picker/image-picker.tsx
+++ src/components/image-picker/image-picker.tsx
@@ -5,6 +5,7 @@ import {
   EventEmitter,
   Host,
   Prop,
+  State,
   h
 } from "@stencil/core";
 
@@ -16,12 +17,22 @@ import { Component as GxComponent } from "../common/interfaces";
   tag: "gx-image-picker"
 })
 export class ImagePicker implements GxComponent {
+  // Used to read the images
+  private reader = new FileReader();
+
+  // Refs
+  private input: HTMLInputElement;
+  private modal: HTMLGxModalElement;
+
+  private dismissTimer: NodeJS.Timeout = null;
+  private shouldShowImagePickerButton: boolean;
+
   @Element() element: HTMLGxImagePickerElement;
 
   /**
    * This attribute lets you specify the alternative text.
    */
-  @Prop({ reflect: true }) alt = "";
+  @Prop({ mutable: true, reflect: true }) alt = "";
 
   /**
    * If true, the component will be sized to match the image's intrinsic size when not constrained
@@ -78,7 +89,7 @@ export class ImagePicker implements GxComponent {
   /**
    * This attribute lets you specify the SRC.
    */
-  @Prop() src = "";
+  @Prop({ mutable: true }) src = "";
 
   /**
    * True to highlight control when an action is fired.
@@ -101,7 +112,7 @@ export class ImagePicker implements GxComponent {
    * This attribute lets you specify the description of the
    * change image button in the modal.
    */
-  @Prop() readonly changeButtonText = "Change image...";
+  @Prop() readonly changeButtonText = "Change image";
 
   /**
    * This attribute lets you specify the description of the
@@ -139,14 +150,7 @@ export class ImagePicker implements GxComponent {
    */
   @Event() onImageChanged: EventEmitter<File>;
 
-  // Used to read the images
-  private reader = new FileReader();
-
-  private input: HTMLInputElement;
-
-  private modal: HTMLGxModalElement;
-
-  private shouldShowImagePickerButton: boolean;
+  @State() renderModalElements = false;
 
   private stopPropagation(event: UIEvent) {
     event.stopPropagation();
@@ -163,6 +167,8 @@ export class ImagePicker implements GxComponent {
     if (this.src === "") {
       this.input.click();
     } else {
+      clearTimeout(this.dismissTimer);
+      this.renderModalElements = true;
       this.modal.opened = true;
     }
     event.stopPropagation();
@@ -180,6 +186,10 @@ export class ImagePicker implements GxComponent {
   // When the modal closes
   private closeAction = () => {
     this.modal.opened = false;
+
+    this.dismissTimer = setTimeout(() => {
+      this.renderModalElements = false;
+    }, 300);
   };
 
   private getFileNameWithoutExtension(fileName: string) {
@@ -243,14 +253,7 @@ export class ImagePicker implements GxComponent {
   // SVG used to display when an image is being uploaded to the server
   private getLoadingSVG(): any {
     return (
-      <svg
-        version="1.1"
-        id="loader-1"
-        xmlns="http://www.w3.org/2000/svg"
-        x="0px"
-        y="0px"
-        viewBox="5 5 40 40"
-      >
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="5 5 40 40">
         <path
           fill="rgba(96, 96, 96, 0.6)"
           d="M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z"
@@ -312,36 +315,67 @@ export class ImagePicker implements GxComponent {
           )}
         </gx-image>
         <gx-modal
-          class="action-dialog"
+          onClose={this.closeAction}
           onClick={this.stopPropagation}
           ref={el => (this.modal = el as HTMLGxModalElement)}
         >
-          <div slot="header">
-            {this.modalTitle === null ? document.title : this.modalTitle}
-          </div>
           <div class="body-container" slot="body">
-            <label class="file">
+            <label class="picker-container">
               <input
+                role="button"
+                aria-label={this.changeButtonText}
+                class="file-picker"
                 type="file"
                 accept="image/*"
                 onChange={this.fileSelectedAction}
                 ref={el => (this.input = el as HTMLInputElement)}
               />
-              <span class="file-custom">{this.changeButtonText}</span>
+              <svg
+                aria-hidden="true"
+                class="download"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 1200 1200"
+              >
+                <path d="M0,1037.516h1200V1200H0V1037.516z M820.785,0h-441.57v496.632H103.233  L600,959.265l496.768-462.633H820.785V0z" />
+              </svg>
+              <span aria-hidden="true" class="custom-file-picker">
+                {this.changeButtonText}
+              </span>
             </label>
-            <gx-button
-              class="gx-button-fit-content"
-              cssClass="remove-button"
-              onClick={this.clearImageAction}
+            {this.renderModalElements && (
+              <button
+                class="remove-image-button"
+                type="button"
+                onClick={this.clearImageAction}
+              >
+                <svg
+                  aria-hidden="true"
+                  class="trash-can"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M 10 2 L 9 3 L 5 3 C 4.448 3 4 3.448 4 4 C 4 4.552 4.448 5 5 5 L 7 5 L 17 5 L 19 5 C 19.552 5 20 4.552 20 4 C 20 3.448 19.552 3 19 3 L 15 3 L 14 2 L 10 2 z M 5 7 L 5 20 C 5 21.105 5.895 22 7 22 L 17 22 C 18.105 22 19 21.105 19 20 L 19 7 L 5 7 z" />
+                </svg>
+                {this.removeButtonText}
+              </button>
+            )}
+          </div>
+
+          {this.renderModalElements && [
+            // The header is below the body slot, because Stencil's
+            // optimizations cause some issues when removing this element
+            <span slot="header">
+              {this.modalTitle === null ? document.title : this.modalTitle}
+            </span>,
+            <button
+              class="cancel-button"
+              type="button"
+              slot="secondary-action"
+              onClick={this.closeAction}
             >
-              {this.removeButtonText}
-            </gx-button>
-          </div>
-          <div slot="secondary-action">
-            <gx-button cssClass="cancel-button" onClick={this.closeAction}>
-              <span>{this.cancelButtonText}</span>
-            </gx-button>
-          </div>
+              {this.cancelButtonText}
+            </button>
+          ]}
         </gx-modal>
       </Host>
     );
