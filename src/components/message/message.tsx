7584935282ed131e9b4b3f24b87beaf231906539
Nicolás Cámera <nico_camera98@hotmail.com>
2022-07-28 15:03:42 -0300

    Remove the old implementation of gx-message

--- src/components/message/message.tsx
+++ src/components/message/message.tsx
@@ -1,63 +1,21 @@
-import { Component, Element, Prop, h, Host } from "@stencil/core";
-import { MessageRender } from "../renders/bootstrap/message/message-render";
-import {
-  Component as GxComponent,
-  VisibilityComponent
-} from "../common/interfaces";
+import { Component, Element, Host, Prop, h } from "@stencil/core";
+import { Component as GxComponent } from "../common/interfaces";
 
 @Component({
   shadow: false,
   styleUrl: "message.scss",
   tag: "gx-message"
 })
-export class Message implements GxComponent, VisibilityComponent {
-  constructor() {
-    this.renderer = new MessageRender(this);
-  }
-
-  private renderer: MessageRender;
-
+export class Message implements GxComponent {
   @Element() element: HTMLGxMessageElement;
 
   /**
-   * This attribute lets you specify how this element will behave when hidden.
-   *
-   * | Value        | Details                                                                     |
-   * | ------------ | --------------------------------------------------------------------------- |
-   * | `keep-space` | The element remains in the document flow, and it does occupy space.         |
-   * | `collapse`   | The element is removed form the document flow, and it doesn't occupy space. |
-   */
-  @Prop() readonly invisibleMode: "collapse" | "keep-space" = "collapse";
-
-  /**
-   * Show a button for closing the meesage box
-   */
-  @Prop() readonly showCloseButton: boolean;
-
-  /**
-   * Text for the close button.
-   */
-  @Prop() readonly closeButtonText: string;
-
-  /**
-   * Type of the button:
-   * * `info`: Information message
-   * * `warning`: Warning Message
-   * * `error`: Error message
-   */
-  @Prop() readonly type: "info" | "warning" | "error";
-
-  /**
-   * The time in miliseconds before the message is automatically dismissed.
+   * The time in seconds before the message is automatically dismissed.
    * If no duration is specified, the message will not be automatically dismissed.
    */
-  @Prop() readonly duration: number;
-
-  componentDidLoad() {
-    this.renderer.componentDidLoad();
-  }
+  @Prop() readonly duration: number = 0;
 
   render() {
-    return <Host>{this.renderer.render({ default: <slot /> })}</Host>;
+    return <Host></Host>;
   }
 }
