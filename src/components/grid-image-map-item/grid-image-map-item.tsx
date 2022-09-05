7db89aee6a15797b5c98796df7638a45295ed358
Nicolás Cámera <nico_camera98@hotmail.com>
2022-09-05 15:53:22 -0300

    Add gx-grid-image-map-item control
     - This control is used to implement the cells of the gx-grid-image-map.
    
     - This control has "position: absolute", relative to the image map.

---
+++ src/components/grid-image-map-item/grid-image-map-item.tsx
@@ -0,0 +1,131 @@
+import {
+  Component,
+  Element,
+  Prop,
+  h,
+  Host,
+  Event,
+  EventEmitter,
+  State
+} from "@stencil/core";
+
+import { Component as GxComponent } from "../common/interfaces";
+import {
+  HighlightableComponent,
+  makeHighlightable
+} from "../common/highlightable";
+
+// Class transforms
+import { getClassesWithoutFocus } from "../common/css-transforms/css-transforms";
+
+@Component({
+  tag: "gx-grid-image-map-item",
+  styleUrl: "grid-image-map-item.scss",
+  shadow: false
+})
+export class GridImageMapItem implements GxComponent, HighlightableComponent {
+  constructor() {
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  @Element() element: HTMLGxGridImageMapItemElement;
+
+  @State() display = true;
+
+  /**
+   * A CSS class to set as the `gx-grid-image-map-item` element class.
+   */
+  @Prop() readonly cssClass: string;
+
+  /**
+   * This attributes lets you specify the item's height.
+   * This attribute maps directly to the `height` CSS property.
+   */
+  @Prop() readonly height: string = "100px";
+
+  /**
+   * True to highlight control when an action is fired.
+   */
+  @Prop() readonly highlightable: boolean = false;
+
+  /**
+   * Defines the left position of the control which is relative to the position
+   * of its `gx-grid-image-map` container.
+   * This attribute maps directly to the `left` CSS property.
+   */
+  @Prop() readonly left: string = "0";
+
+  /**
+   * This attributes lets you specify the item's rotation.
+   * This attribute maps directly to the `rotation` CSS property.
+   */
+  @Prop() readonly rotation: string = null;
+
+  /**
+   * Defines the top position of the control which is relative to the position
+   * of its `gx-grid-image-map` container.
+   * This attribute maps directly to the `top` CSS property.
+   */
+  @Prop() readonly top: string = "0";
+
+  /**
+   * This attributes lets you specify the item's width.
+   * This attribute maps directly to the `width` CSS property.
+   */
+  @Prop() readonly width: string = "100px";
+
+  /**
+   * Emitted when the element is clicked.
+   */
+  @Event() gxClick: EventEmitter;
+
+  private handleClick(event: UIEvent) {
+    this.gxClick.emit(event);
+  }
+
+  componentDidLoad() {
+    makeHighlightable(this);
+  }
+
+  /**
+   * @todo TODO: Try to reproduce this case and check if there is necessary.
+   */
+  componentWillLoad() {
+    //Do not show item if item size is bigger than screen size
+    if (
+      window.screen.width >= Number(this.width) ||
+      window.screen.height >= Number(this.height)
+    ) {
+      this.display = true;
+    } else {
+      this.display = false;
+    }
+  }
+
+  render() {
+    const classes = getClassesWithoutFocus(this.cssClass);
+    this.element.addEventListener("click", this.handleClick);
+
+    return (
+      <Host
+        class={{
+          "no-display": this.display === false,
+          [this.cssClass]: !!this.cssClass,
+          [classes.vars]: true
+        }}
+        style={{
+          top: `calc(${this.top}px * (var(--image-map-height) / var(--image-map-image-height)))`,
+          left: `calc(${this.left}px * (var(--image-map-width) / var(--image-map-image-width)))`,
+          width: `calc(${this.width}px * (var(--image-map-width) / var(--image-map-image-width)))`,
+          minHeight: `calc(${this.height}px * (var(--image-map-height) / var(--image-map-image-height)))`,
+          transform:
+            this.rotation != null && this.rotation !== "0"
+              ? `rotate(${this.rotation}deg)`
+              : null
+        }}
+      >
+        <slot />
+      </Host>
+    );
+  }
+}
---
+++ src/components/grid-image-map-item/readme.md
@@ -0,0 +1,25 @@
+# gx-image-map-item
+
+<!-- Auto Generated Below -->
+
+## Properties
+
+| Property        | Attribute       | Description                                                                                                                                                               | Type      | Default     |
+| --------------- | --------------- | ------------------------------------------------------------------------------------------------------------------------------------------------------------------------- | --------- | ----------- |
+| `cssClass`      | `css-class`     | A CSS class to set as the `gx-grid-image-map-item` element class.                                                                                                         | `string`  | `undefined` |
+| `height`        | `height`        | This attributes lets you specify the item's height. This attribute maps directly to the `height` CSS property.                                                            | `string`  | `"100px"`   |
+| `highlightable` | `highlightable` | True to highlight control when an action is fired.                                                                                                                        | `boolean` | `false`     |
+| `left`          | `left`          | Defines the left position of the control which is relative to the position of its `gx-grid-image-map` container. This attribute maps directly to the `left` CSS property. | `string`  | `"0"`       |
+| `rotation`      | `rotation`      | This attributes lets you specify the item's rotation. This attribute maps directly to the `rotation` CSS property.                                                        | `string`  | `null`      |
+| `top`           | `top`           | Defines the top position of the control which is relative to the position of its `gx-grid-image-map` container. This attribute maps directly to the `top` CSS property.   | `string`  | `"0"`       |
+| `width`         | `width`         | This attributes lets you specify the item's width. This attribute maps directly to the `width` CSS property.                                                              | `string`  | `"100px"`   |
+
+## Events
+
+| Event     | Description                          | Type               |
+| --------- | ------------------------------------ | ------------------ |
+| `gxClick` | Emitted when the element is clicked. | `CustomEvent<any>` |
+
+---
+
+_Built with [StencilJS](https://stenciljs.com/)_
