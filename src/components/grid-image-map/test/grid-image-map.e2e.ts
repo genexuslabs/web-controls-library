2dfa8ad4f97ab301c8dfa9009a2eee715312eae0
Nicolás Cámera <81186472+ncamera@users.noreply.github.com>
2022-09-08 17:48:12 -0300

    Add support for Image Map Grid control (#279)
    * Add gx-grid-image-map control
    
     - Currently, the gx-grid-image-map control does not support auto-grow.
    
     - A tooltip is displayed when the mouse is over the control to tell the user that the control can be zoomed in or out.
    
     - The gx-grid-image-map control uses an external library (custom-pinch-zoom-element) to implement both desktop and mobile zoom use cases . In the future, it would be nice to make our own custom function to implement the zoom use cases.
    
     - LongPress support is limited to emitting the event without the coordinates.
    
     - At this moment, the "grid-empty-loading-placeholder" and "grid-content-empty" slots are inside the pinch-zoom element (otherwise, the pinch-zoom does not work), but they should be outside.
    
    * Add gx-grid-image-map-item control
    
     - This control is used to implement the cells of the gx-grid-image-map.
    
     - This control has "position: absolute", relative to the image map.

---
+++ src/components/grid-image-map/test/grid-image-map.e2e.ts
@@ -0,0 +1,107 @@
+import { newE2EPage, E2EElement, E2EPage } from "@stencil/core/testing";
+import { cleanMeasureUnit } from "./utils";
+
+describe("gx-grid-image-map", () => {
+  let element: E2EElement;
+  let page: E2EPage;
+
+  const src = "https://i.ibb.co/pPgZrLt/image-2021-12-01-14-07-01.png";
+
+  beforeEach(async () => {
+    page = await newE2EPage();
+  });
+
+  it("renders", async () => {
+    await page.setContent(
+      `<gx-grid-image-map src='${src}'></gx-grid-image-map>`
+    );
+    await page.waitForChanges();
+    element = await page.find("gx-grid-image-map");
+    await page.waitForChanges();
+    expect(element).toHaveClass("hydrated");
+  });
+
+  it("set background image", async () => {
+    await page.setContent(
+      `<gx-grid-image-map src='${src}'></gx-grid-image-map>`
+    );
+    await page.waitForChanges();
+
+    const backgroundImage = await page.find(".gx-image-map-container>img");
+    await page.waitForChanges();
+
+    ///checks if background is set to img tag
+    expect(backgroundImage.outerHTML).toContain(src);
+  });
+
+  it("trigger gxZoom event", async () => {
+    await page.setContent(
+      `<gx-grid-image-map src='${src}'></gx-grid-image-map>`
+    );
+
+    const gxZoom = await page.spyOnEvent("gxZoom");
+
+    element = await page.find("gx-grid-image-map");
+
+    element.triggerEvent("wheel");
+    await page.waitForChanges();
+    expect(gxZoom).toHaveReceivedEvent();
+  });
+
+  it("show tooltip when tooltip-text property is set and mouse is over", async () => {
+    let tooltip: E2EElement = null;
+    await page.setContent(
+      `<gx-grid-image-map src='${src}' tooltip-text="Example"></gx-grid-image-map>`
+    );
+    await page.waitForChanges();
+    element = await page.find("gx-grid-image-map");
+    await element.hover();
+    tooltip = await page.find(".tooltip-text");
+    const visible = await tooltip.isVisible();
+    expect(visible).toBe(true);
+  });
+
+  it("do not show tooltip by default", async () => {
+    let tooltip: E2EElement = null;
+    await page.setContent(
+      `<gx-grid-image-map src='${src}'></gx-grid-image-map>`
+    );
+    element = await page.find("gx-grid-image-map");
+    await element.hover();
+    tooltip = await page.find(".tooltip-text");
+    expect(tooltip).toBeNull();
+  });
+
+  it("remove tooltip after zoom", async () => {
+    let tooltip: E2EElement = null;
+    await page.setContent(
+      `<gx-grid-image-map src='${src}' tooltip-text="Example"></gx-grid-image-map>`
+    );
+    await page.waitForChanges();
+    element = await page.find("gx-grid-image-map");
+    tooltip = await page.find(".tooltip-text");
+    expect(tooltip).toBeDefined();
+    element.triggerEvent("wheel");
+    await page.waitForChanges();
+    expect(await page.find(".tooltip-text")).toBeFalsy();
+  });
+
+  it("gx-grid-image-map stretches to the width of the parent", async () => {
+    const parentWidth = 300;
+
+    await page.setContent(
+      `<div style="width:${parentWidth}px">
+        <gx-grid-image-map src='${src}'>
+        </gx-grid-image-map>
+      </div>`
+    );
+    element = await page.find("gx-grid-image-map");
+    await page.waitForChanges();
+
+    const componentWidth = Number(
+      cleanMeasureUnit((await element.getComputedStyle()).width)
+    );
+
+    expect(componentWidth).toEqual(parentWidth);
+  });
+});
---
+++ src/components/grid-image-map/test/utils.ts
@@ -0,0 +1,9 @@
+export function cleanMeasureUnit(value: string) {
+  if (value.length != 0) {
+    return value.slice(0, value.length - 2);
+  }
+}
+
+export const getAspectRatio = (width: number, height: number): number => {
+  return Number((width / height).toFixed(1));
+};
