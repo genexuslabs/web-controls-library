beafb917eb04ed183c6961786129568a71f79fd6
Nicolás Cámera <ncamera@genexus.com>
2022-10-25 17:42:11 -0300

    Refactoring for gx-button implementation
     - gx-button will no longer used the native <button> tag to implement its behavior, but will add the role="button" and implement the corresponding handlers (click, enter key and space key) to emulate the behavior of a button.
    
     - Improved control accessibility (aria-disabled is added when the control is disabled).
    
     - The control will be aligned by its parent cell.
    
     - Added support for Shadow DOM
    
     - The complexity of CSS and HTML has been reduced.
    
     - Simplified the implementation of image positioning.

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,7 +1,16 @@
-import { Component, Element, Listen, Prop, h } from "@stencil/core";
 import {
-  DisableableComponent,
+  Component,
+  Element,
+  Event,
+  EventEmitter,
+  Prop,
+  h,
+  Host
+} from "@stencil/core";
+import {
   Component as GxComponent,
+  CustomizableComponent,
+  DisableableComponent,
   VisibilityComponent
 } from "../common/interfaces";
 import {
@@ -9,26 +18,42 @@ import {
   makeHighlightable
 } from "../common/highlightable";
 
-import { ButtonRender } from "../renders/bootstrap/button/button-render";
+import { imagePositionClass } from "../common/image-position";
 
+// Class transforms
+import { getClasses } from "../common/css-transforms/css-transforms";
+
+const ENTER_KEY_CODE = "Enter";
+const SPACE_KEY_CODE = "Space";
+
+/**
+ * @part caption - The caption displayed at the center of the control.
+ *
+ * @slot - The slot for the caption displayed.
+ * @slot main-image - The slot for the main `img`.
+ * @slot disabled-image - The slot for the disabled `img`.
+ */
 @Component({
-  shadow: false,
+  shadow: true,
   styleUrl: "button.scss",
   tag: "gx-button"
 })
 export class Button
   implements
     GxComponent,
+    CustomizableComponent,
     DisableableComponent,
-    VisibilityComponent,
-    HighlightableComponent {
-  constructor() {
-    this.renderer = new ButtonRender(this, {
-      handleClick: this.handleClick.bind(this)
-    });
-  }
+    HighlightableComponent,
+    VisibilityComponent {
+  /**
+   * `true` if the button has disabled image.
+   */
+  private hasDisabledImage = false;
 
-  private renderer: ButtonRender;
+  /**
+   * `true` if the button has main image.
+   */
+  private hasMainImage = false;
 
   @Element() element: HTMLGxButtonElement;
 
@@ -74,17 +99,6 @@ export class Button
     | "behind" = "above";
 
   /**
-   * This attribute lets you specify the size of the button.
-   *
-   * | Value    | Details                                                 |
-   * | -------- | ------------------------------------------------------- |
-   * | `large`  | Large sized button.                                     |
-   * | `normal` | Standard sized button.                                  |
-   * | `small`  | Small sized button.                                     |
-   */
-  @Prop() readonly size: "large" | "normal" | "small" = "normal";
-
-  /**
    * True to highlight control when an action is fired.
    */
   @Prop() readonly highlightable = true;
@@ -99,29 +113,105 @@ export class Button
    */
   @Prop() readonly height: string = "";
 
-  @Listen("click", { capture: true })
-  handleClick(event: UIEvent) {
+  /**
+   * Emitted when the element is clicked, the enter key is pressed or the space key is pressed and released.
+   */
+  @Event() click: EventEmitter;
+
+  private handleKeyDown = (event: KeyboardEvent) => {
+    // The action button is activated by space on the keyup event, but the
+    // default action for space is already triggered on keydown. It needs to be
+    // prevented to stop scrolling the page before activating the button.
+    if (event.code === SPACE_KEY_CODE) {
+      event.preventDefault();
+    }
+
+    // If enter is pressed, activate the button
+    if (event.code === ENTER_KEY_CODE) {
+      event.preventDefault();
+      this.handleButtonAction();
+    }
+  };
+
+  private handleKeyUp = (event: KeyboardEvent) => {
+    if (event.code === SPACE_KEY_CODE) {
+      event.preventDefault();
+      this.handleButtonAction();
+    }
+  };
+
+  private handleButtonAction = () => {
+    // In some cases, the button could be disabled between the keydown and keyup
     if (this.disabled) {
-      event.stopPropagation();
       return;
     }
-  }
+    this.click.emit();
+  };
 
   componentWillLoad() {
-    this.renderer.componentWillLoad();
+    const mainImage = this.element.querySelector(
+      ":scope > [slot='main-image']"
+    );
+    const disabledImage = this.element.querySelector(
+      ":scope > [slot='disabled-image']"
+    );
+
+    this.hasMainImage = mainImage !== null;
+    this.hasDisabledImage = disabledImage !== null;
   }
 
   componentDidLoad() {
-    const innerButton = this.element.querySelector("button");
-
-    makeHighlightable(this, innerButton);
+    makeHighlightable(this);
   }
 
   render() {
-    return this.renderer.render({
-      default: <slot />,
-      disabledImage: <slot name="disabled-image" />,
-      mainImage: <slot name="main-image" />
-    });
+    // Styling for gx-button control.
+    const classes = getClasses(this.cssClass);
+
+    /** True if the button does not have any text */
+    const isEmptyCaption = this.element.textContent.trim() === "";
+
+    return (
+      <Host
+        role="button"
+        aria-disabled={this.disabled ? "true" : undefined}
+        class={{
+          [this.cssClass]: !!this.cssClass,
+          [classes.vars]: true,
+          disabled: this.disabled,
+
+          // Strings with only white spaces are taken as null captions
+          "empty-caption": isEmptyCaption,
+
+          [imagePositionClass(this.imagePosition)]:
+            this.hasMainImage || this.hasDisabledImage
+        }}
+        style={{
+          "--width": !!this.width ? this.width : null,
+          "--height": !!this.height ? this.height : null
+        }}
+        data-has-action={!this.disabled ? "" : undefined} // Mouse pointer to indicate action
+        tabindex={this.highlightable && !this.disabled ? "0" : undefined} // Add focus to the control through sequential keyboard navigation and visually clicking
+        // Implement button events
+        onKeyDown={!this.disabled ? this.handleKeyDown : undefined}
+        onKeyUp={!this.disabled ? this.handleKeyUp : undefined}
+      >
+        {!isEmptyCaption && (
+          <span class="caption" part="caption">
+            <slot />
+          </span>
+        )}
+
+        {// Main image
+        this.hasMainImage && (!this.disabled || !this.hasDisabledImage) && (
+          <slot name="main-image" aria-hidden="true" />
+        )}
+
+        {// Disabled image
+        this.hasDisabledImage && this.disabled && (
+          <slot name="disabled-image" aria-hidden="true" />
+        )}
+      </Host>
+    );
   }
 }
