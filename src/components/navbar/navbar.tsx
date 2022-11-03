9f13153744e3c98e721c5d3fb791023a19e8aa6f
Nicolás Cámera <ncamera@genexus.com>
2022-11-03 12:51:12 -0300

    Update gx-navbar part names and classes

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -18,6 +18,12 @@ import {
   attachHorizontalScrollWithWheelHandler
 } from "../common/utils";
 
+/**
+ * @part back-button - The back button displayed on the left side of the control.
+ * @part default-button - The toggle button displayed on the left side of the control.
+ * @part low-actions-dropdown - The dropdown displayed on the right side of the control when clicking the "show more" button ("low-actions-toggle" part).
+ * @part low-actions-toggle - The "show more" button displayed on the right side of the control when the navbar has low priority actions.
+ */
 @Component({
   shadow: true,
   styleUrl: "navbar.scss",
@@ -342,7 +348,7 @@ export class NavBar implements GxComponent {
       this.hasLowPriorityActions && (
         <div class="gx-navbar-actions-low">
           <div
-            part="action-low-popup"
+            part="low-actions-dropdown"
             class={{
               "gx-navbar-actions-low-popup": true,
               "gx-navbar-actions-low-popup--active": this.showLowActions
@@ -353,7 +359,7 @@ export class NavBar implements GxComponent {
           <button
             type="button"
             aria-label={this.actionToggleButtonLabel}
-            part="action-low"
+            part="low-actions-toggle"
             class={{
               "gx-navbar-actions-low-toggle": true,
               "gx-navbar-actions-low-toggle--active": this.showLowActions
