25da8940ad88080505a46fd911ba6cbb8e161713
Nicolás Cámera <ncamera@genexus.com>
2023-03-24 15:25:50 -0300

    Update and improve linter rules
    Removed "prettier/@typescript-eslint" from the "extends" array, since this rules has been merged into "prettier" in eslint-config-prettier 8.0.0.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,19 @@ module.exports = {
   plugins: ["@typescript-eslint", "local"],
   extends: [
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-    "plugin:@stencil/recommended" // Enables @stencil/eslint-plugin.
+    "plugin:@stencil-community/recommended", // Enables @stencil-community/eslint-plugin.
+    "plugin:prettier/recommended" // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   parserOptions: {
-    project: "./tsconfig.json"
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    project: "./tsconfig.json",
+    sourceType: "module" // Allows for the use of imports
   },
   rules: {
+    "no-undef": "off", // Allows defining undefined variables
+    "no-console": "warn",
+
+    "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-use-before-define": ["error", { functions: false }],
     "@typescript-eslint/no-unused-vars": [
       "error",
@@ -18,14 +23,42 @@ module.exports = {
     ],
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-explicit-any": "off",
+
     "prettier/prettier": [
       "error",
       {
         endOfLine: "auto"
       }
     ],
-    "@stencil/strict-boolean-conditions": ["error", ["allow-string"]],
-    "@stencil/strict-mutable": "off"
+
+    "@stencil-community/async-methods": "error", // This rule catches Stencil public methods that are not async
+    "@stencil-community/decorators-context": "error", // This rule catches Stencil decorators in bad locations
+    "@stencil-community/decorators-style": [
+      "error",
+      {
+        prop: "inline",
+        state: "inline",
+        element: "inline",
+        method: "multiline",
+        watch: "multiline",
+        listen: "multiline"
+      }
+    ],
+    "@stencil-community/element-type": "error", // This rule catches Stencil Element decorator have the correct type
+    "@stencil-community/methods-must-be-public": "error", // This rule catches Stencil Methods marked as private or protected
+    "@stencil-community/no-unused-watch": "error", // This rule catches Stencil Watchs with non existing Props or States
+    "@stencil-community/own-methods-must-be-private": "error", // This rule catches own class methods marked as public
+    "@stencil-community/own-props-must-be-private": "error", // This rule catches own class properties marked as public
+    "@stencil-community/prefer-vdom-listener": "error", // This rule catches Stencil Listen with vdom events
+    "@stencil-community/props-must-be-public": "error", // This rule catches Stencil Props marked as private or protected
+    "@stencil-community/props-must-be-readonly": "error", // This rule catches Stencil Props marked as non readonly, excluding mutable ones
+    "@stencil-community/required-jsdoc": "error", // This rule catches Stencil Props, Methods and Events to define jsdoc
+    "@stencil-community/required-prefix": ["error", ["gx-"]], // Ensures that a Component's tag use the "gx-" prefix.
+    "@stencil-community/single-export": "error", // This rule catches modules that expose more than just the Stencil Component itself
+    "@stencil-community/strict-boolean-conditions": "off",
+
+    // WA to fix false positive errors
+    "@stencil-community/strict-mutable": "warn" // This rule catches Stencil Prop marked as mutable but not changing value in code
   },
   overrides: [
     {
