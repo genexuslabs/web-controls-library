b2c5eedbcfff1773e1ddf97539767a08dfb59590
Nicolás Cámera <ncamera@genexus.com>
2023-07-20 23:35:12 -0300

    Add support for Accessible Role in container cells

--- src/common/interfaces.ts
+++ src/common/interfaces.ts
@@ -7,6 +7,8 @@ export type AccessibleRole =
   | "main"
   | "region";
 
+export type AccessibleRoleCell = "listitem";
+
 export interface AccessibleRoleComponent {
   /**
    * Specifies the semantics of the control. Specifying the Role allows
@@ -14,4 +16,13 @@ export interface AccessibleRoleComponent {
    * the user.
    */
   accessibleRole: AccessibleRole;
+}
+
+export interface AccessibleRoleCellComponent {
+  /**
+   * Specifies the semantics of the control. Specifying the Role allows
+   * assistive technologies to give information about how to use the control to
+   * the user.
+   */
+  accessibleRole: AccessibleRoleCell;
 }
