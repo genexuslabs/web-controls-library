7a0cd96c5f7fb246f75d1cff3e1beb89d680a934
Nicolás Cámera <ncamera@genexus.com>
2023-08-03 11:57:47 -0300

    Remove checkbox's old implementation

--- src/components/checkbox/checkbox.tsx
+++ src/components/checkbox/checkbox.tsx
@@ -3,34 +3,23 @@ import {
   Element,
   Event,
   EventEmitter,
-  Host,
   Method,
   Prop,
   Watch,
+  Host,
   h
 } from "@stencil/core";
-import { FormComponent } from "../common/interfaces";
+import { DisableableComponent } from "../common/interfaces";
 
 import { AccessibleNameComponent } from "../../common/interfaces";
-
-import { CheckBoxRender } from "../renders/bootstrap/checkbox/checkbox-render";
-
-// Class transforms
-import { getClasses } from "../common/css-transforms/css-transforms";
 
 @Component({
   shadow: false,
   styleUrl: "checkbox.scss",
   tag: "gx-checkbox"
 })
-export class CheckBox implements AccessibleNameComponent, FormComponent {
-  constructor() {
-    this.renderer = new CheckBoxRender(this, {
-      handleChange: this.handleChange.bind(this)
-    });
-  }
-
-  private renderer: CheckBoxRender;
+export class CheckBox implements AccessibleNameComponent, DisableableComponent {
+  private checkboxId: string;
 
   @Element() element: HTMLGxCheckboxElement;
 
@@ -74,16 +63,6 @@ export class CheckBox implements AccessibleNameComponent, FormComponent {
   @Prop() readonly highlightable: boolean = false;
 
   /**
-   * This attribute lets you specify how this element will behave when hidden.
-   *
-   * | Value        | Details                                                                     |
-   * | ------------ | --------------------------------------------------------------------------- |
-   * | `keep-space` | The element remains in the document flow, and it does occupy space.         |
-   * | `collapse`   | The element is removed form the document flow, and it doesn't occupy space. |
-   */
-  @Prop() readonly invisibleMode: "collapse" | "keep-space" = "collapse";
-
-  /**
    * This attribute indicates that the user cannot modify the value of the control.
    * Same as [readonly](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#attr-readonly)
    * attribute for `input` elements.
@@ -101,7 +80,8 @@ export class CheckBox implements AccessibleNameComponent, FormComponent {
   @Prop({ mutable: true }) value: string;
 
   /**
-   * The `input` event is emitted when a change to the element's value is committed by the user.
+   * The `input` event is emitted when a change to the element's value is
+   * committed by the user.
    */
   @Event() input: EventEmitter;
 
@@ -110,16 +90,7 @@ export class CheckBox implements AccessibleNameComponent, FormComponent {
    */
   @Method()
   async getNativeInputId() {
-    return this.renderer.getNativeInputId();
-  }
-
-  @Watch("checked")
-  protected checkedChanged() {
-    this.renderer.checkedChanged();
-  }
-
-  componentWillLoad() {
-    this.checked = this.value === this.checkedValue;
+    return this.checkboxId;
   }
 
   @Watch("value")
@@ -127,39 +98,14 @@ export class CheckBox implements AccessibleNameComponent, FormComponent {
     this.checked = this.value === this.checkedValue;
   }
 
-  private handleChange(event: UIEvent) {
-    event.stopPropagation();
-    this.checked = this.renderer.getValueFromEvent(event);
-    this.updateValue();
-    this.input.emit(event);
-  }
-
-  private updateValue() {
-    this.value = this.checked ? this.checkedValue : this.unCheckedValue;
-  }
-
   render() {
-    // Styling for gx-checkbox control.
-    const classes = getClasses(this.cssClass);
-
     return (
       <Host
         class={{
           [this.cssClass]: !!this.cssClass,
-          [classes.vars]: true,
-          disabled: this.disabled
+          [DISABLED_CLASS]: this.disabled
         }}
-        // Mouse pointer to indicate action
-        data-has-action={this.highlightable && !this.disabled ? "" : undefined}
-        // Add focus to the control through sequential keyboard navigation and visually clicking
-        tabindex={
-          this.highlightable && this.readonly && !this.disabled
-            ? "0"
-            : undefined
-        }
-      >
-        {this.renderer.render()}
-      </Host>
+      ></Host>
     );
   }
 }
